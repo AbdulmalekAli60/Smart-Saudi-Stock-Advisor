d6022aed505bc019a8f7424beb48e1fbf90006fb
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2025-06-26 14:16:33 +0300

    Initial commit, project structure setup

---
+++ backend/SmartSaudiStockAdvisor/SmartSaudiStockAdvisor/src/main/java/com/SmartSaudiStockAdvisor/SmartSaudiStockAdvisorApplication.java
@@ -0,0 +1,13 @@
+package com.SmartSaudiStockAdvisor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSaudiStockAdvisorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSaudiStockAdvisorApplication.class, args);
+	}
+
+}
---
+++ backend/SmartSaudiStockAdvisor/SmartSaudiStockAdvisor/src/test/java/com/SmartSaudiStockAdvisor/SmartSaudiStockAdvisorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SmartSaudiStockAdvisor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSaudiStockAdvisorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
