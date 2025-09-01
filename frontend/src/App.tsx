9791bc8a35ecce932121b7c3e75b920c57c3cceb
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2025-09-01 21:03:53 +0300

    added fonts and colors

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,11 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <div className='rounded-2xl bg-red-500'>Hi</div>
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
