52152058d3532d3d066cbdb28bfc2b62314de598
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2025-10-12 20:01:32 +0300

    working on axios interceptor

---
+++ frontend/src/services/RefreshToken.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosError } from "axios";
+
+
+// axios.interceptors.response.use(
+//     function(response) {
+//         return response;
+//     }
+
+//     async function (error: AxiosError) {
+//         const originalRequest = error.config
+        
+//         if(error.response?.status === 401){
+
+//         }
+//     }
+// ) 
