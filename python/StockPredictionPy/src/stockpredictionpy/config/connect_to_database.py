88eeb7c09bd7db149aead33c2b4c6487d53a5b4f
AbdulmalekAli60 <ZxshxZ123@gmail.com>
2026-01-19 14:43:01 +0300

    Dockerfile

--- python/StockPredictionPy/src/stockpredictionpy/config/connect_to_database.py
+++ python/StockPredictionPy/src/stockpredictionpy/config/connect_to_database.py
@@ -1,7 +1,7 @@
 import psycopg2
 import logging
-from config.databaseConnInfo import DATABASE, HOST, PASSWORD, PORT, USER
-
+# from config.databaseConnInfo import DATABASE, HOST, PASSWORD, PORT, USER
+import os
 logger = logging.getLogger('database_connection')
 logger.setLevel(logging.INFO)
 
@@ -15,13 +15,20 @@ if not logger.handlers:
     logger.addHandler(file_handler)
 
 def connect_to_database() -> psycopg2.extensions.connection | None:
+        
+        db_host = os.getenv('DB_HOST', 'localhost')
+        db_port = os.getenv('DB_PORT', '5432')
+        db_name = os.getenv('DB_NAME', 'SmartSaudiStockAdvisor')
+        db_user = os.getenv('DB_USER', 'postgres')
+        db_password = os.getenv('DB_PASSWORD', 'Afsd1423')
+
         try:
-            conn:psycopg2.extensions.connection = psycopg2.connect(
-                database=DATABASE,
-                user=USER,
-                host=HOST,
-                password=PASSWORD,
-                port=PORT
+            conn = psycopg2.connect(
+                host=db_host,
+                port=db_port,
+                dbname=db_name,
+                user=db_user,
+                password=db_password
             )
             logger.info("Successfully connected to database")
             return conn
